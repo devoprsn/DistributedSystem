a125111cd1575c662c4e8abd2bcb762096c0eab8
devoprsn <devoprsn@gmail.com>
2018-04-24 13:15:07 -0400

    Added to TreadedServer.java

--- src/ThreadedServer.java
+++ src/ThreadedServer.java
@@ -1,23 +1,36 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class ThreadedServer implements Runnable{
 	
 	int portNumber;
-	public ThreadedServer(int portNumber)
+	LinkedList<Job> jobs;
+	public ThreadedServer(int portNumber, LinkedList<Job> jobs)
 	{
 		this.portNumber = portNumber;
+		this.jobs = jobs;
 	}
 	
 	@Override
 	public void run() 
-	{
-		try (ServerSocket serverSocket = new ServerSocket(portNumber);) {
-			
+	{		
+		int portNumber = Integer.parseInt("");
+		final int THREADS = 3;		
+		
+		try (ServerSocket serverSocket = new ServerSocket(portNumber);) 
+		{
+			ArrayList<Thread> threads = new ArrayList<Thread>();
+			for (int i = 0; i < THREADS; i++)
+				threads.add(new Thread(new ServerThread(serverSocket, i, jobs)));
+			for (Thread t : threads)
+				t.start();
 		} 
 		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println(
+					"Exception caught when trying to listen on port " + "30122"  + " or listening for a connection");
+			System.out.println(e.getMessage());
 		}
 		
 	}
