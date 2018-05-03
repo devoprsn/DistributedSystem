a4ae071cfd1c7c21aed65675de9d1cb865b0f3bd
Adina Margolis <adinazissel@gmail.com>
2018-05-03 14:27:21 -0400

    Adding slave class and corrected clientServerThread

--- src/Slave.java
+++ src/Slave.java
@@ -6,30 +6,34 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
 
-public class ServerThread implements Runnable {
+//where will we put the code for the slave to actually carry out the work - the slave itself may need a worker thread to work while communicating...
+
+public class Slave {
+
+	private ServerSocket serverSocket;
+	private LinkedList<Job> tasks = new LinkedList<Job>();
 	
-	private ServerSocket serverSocket = null;
-	int id; 
-	LinkedList<Job> jobs;
-	public ServerThread(ServerSocket s, int id, LinkedList<Job> jobs)
+	public Slave(int portNumber)
 	{
-		serverSocket = s;
-		this.id = id;
-		this.jobs = jobs;
-	}
-	
-	@Override
-	public void run() 
-	{
+		try{
+		serverSocket = new ServerSocket(portNumber);
+		}
+		catch(IOException e)
+		{
+		        System.out.println(e.getMessage());
+		}
+		
+
 		try (Socket clientSocket = serverSocket.accept();
 				PrintWriter responseWriter = new PrintWriter(clientSocket.getOutputStream(), true);
 				BufferedReader requestReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) 
 		{
 			String requestString;
-			while ((requestString = requestReader.readLine()) != null) 
+			while ((requestString = requestReader.readLine()) != null) //really needs to receive a job
 			{				
-				responseWriter.println("Request has been received by ");
-				Job job = new Job(new RandomValueGenerator());
+				
+				tasks.add(job);
+				
 				
 			}
 		} 
