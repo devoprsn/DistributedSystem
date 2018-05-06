c737b68e0e70c7e58116cf9be25d04e02d87fdfa
dklahr@student.touro.edu <dklahr@student.touro.edu>
2018-05-05 23:51:55 -0400

    updated Slave

--- src/Slave.java
+++ src/Slave.java
@@ -10,37 +10,49 @@ import java.util.LinkedList;
 
 public class Slave {
 
-	private ServerSocket serverSocket;
-	private LinkedList<Job> tasks = new LinkedList<Job>();
+	private static IRandomValueGenerator rand;
+
 	
-	public Slave(int portNumber)
+	public Slave(int portNumber, IRandomValueGenerator rand)
 	{
+		this.rand = rand;
+	}
+	
+	public static void main(String[] args)
+	{
+		if (args.length != 1) {
+			System.err.println("Usage: java EchoServer <port number>");
+			System.exit(1);
+		}
+
+		int portNumber = Integer.parseInt(args[0]);
+		ServerSocket serverSocket = null;
 		try{
-		serverSocket = new ServerSocket(portNumber);
+			serverSocket = new ServerSocket(portNumber);
 		}
 		catch(IOException e)
 		{
 		        System.out.println(e.getMessage());
 		}
-		
-
+			
+		LinkedList<Job> tasks = new LinkedList<Job>();
 		try (Socket clientSocket = serverSocket.accept();
-				PrintWriter responseWriter = new PrintWriter(clientSocket.getOutputStream(), true);
-				BufferedReader requestReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) 
+					PrintWriter responseWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+					BufferedReader requestReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) 
 		{
-			String requestString;
-			while ((requestString = requestReader.readLine()) != null) //really needs to receive a job
+			String jobRequest;
+			while ((jobRequest = requestReader.readLine()) != null) //really needs to receive a job
 			{				
-				
+				Job job = new Job(rand);
 				tasks.add(job);
-				
+					
 				
 			}
-		} 
-		catch (IOException e) {
-			System.out.println(
-					"Exception caught when trying to listen on port " + serverSocket.getLocalPort() + " or listening for a connection");
-			System.out.println(e.getMessage());
-		}
+			} 
+			catch (IOException e) {
+				System.out.println(
+						"Exception caught when trying to listen on port " + serverSocket.getLocalPort() + " or listening for a connection");
+				System.out.println(e.getMessage());
+			}
 	}
 }
