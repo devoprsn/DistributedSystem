9f6e9d2df359f84fb47ca4840c3b569bc2f3e0a6
dklahr@student.touro.edu <dklahr@student.touro.edu>
2018-05-27 13:29:18 -0400

    modified mutual exclusion in loop and added println statements for testing

--- src/SlaveThreadedServer.java
+++ src/SlaveThreadedServer.java
@@ -1,91 +1,82 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class SlaveThreadedServer implements Runnable
-{
-	private String[] iPAddresses;
-	private int [] portNumbers;
-	
+public class SlaveThreadedServer extends Thread
+{	
 	private LinkedList<String> jobs;
+	String[] IPAddresses;
+	int[] portNumbers;
 	private LinkedList<SlaveServerThread> idleSlaves; 
 	private LinkedList<SlaveServerThread> workingSlaves;
 	private ArrayList<SlaveServerThread> slaveThreads;
 	
-	private RedistributingThread redistributeThread;
+//	private RedistributingThread redistributeThread;
 	
-   public SlaveThreadedServer(String[] iPAddresses, int [] portNumbers, LinkedList<String> jobs)
+   public SlaveThreadedServer(LinkedList<String> jobs, String[] IPAddresses, int[] portNumbers)
    {
-	  this.iPAddresses = iPAddresses;
-	  this.portNumbers = portNumbers;
 	  this.jobs = jobs;
-	  
+	  this.IPAddresses = IPAddresses;
+	  this.portNumbers = portNumbers;
 	  idleSlaves = new LinkedList<SlaveServerThread>();
-	  workingSlaves = new LinkedList<SlaveServerThread>();
-	  
-	  slaveThreads = new ArrayList<SlaveServerThread>();
-	  
-	  //instantiate a thread that will take care of load balancing. redistributing the jobs every so often
-	  redistributeThread = new RedistributingThread(idleSlaves,workingSlaves);  
-	  
-	  
+	  workingSlaves = new LinkedList<SlaveServerThread>();  
+	  slaveThreads = new ArrayList<SlaveServerThread>();	  	  
    }
    
 
 	@Override
 	public void run()
 	{
-		final int THREADS = 4;	
+		System.out.println("SlaveThreadedServer initialized"); //println for testing
 		
+		 final int THREADS = IPAddresses.length;	
+			
 		for (int i = 0; i < THREADS; i++)
 		{
-			slaveThreads.add(new SlaveServerThread(iPAddresses[i], portNumbers[i], i , this));
+			slaveThreads.add(new SlaveServerThread(IPAddresses[i], portNumbers[i], i, this));
 		}
 		for (Thread t : slaveThreads)
 		{
 			t.start();
-		}
-		
-		
-		//start all slaves off on the idleSlaves list:		
+		}	
 		idleSlaves.addAll(slaveThreads);
-		
-		//start the redistributing thread:
-		redistributeThread.start();
-		
-		
-		
+			
+		String job;
 		//continuously loop through and check if there are more jobs to give out
 		while(true)
-		{
-			if(!jobs.isEmpty())
-			{
+		   {
+				if(!jobs.isEmpty())
+				{
+					
+				    if (!idleSlaves.isEmpty())
+				    {
+				    	SlaveServerThread idleSlave = idleSlaves.getFirst();				    	
+				    	synchronized(jobs)
+						{	
+				    		idleSlave.addJob(jobs.removeFirst());
+						}
+				    	System.out.println("Sent job to thread "+idleSlave.getID());
+				    	workingSlaves.add(idleSlaves.removeFirst());
+				    }
+				    
+				    else
+				    {
+				    	SlaveServerThread first = workingSlaves.removeFirst();
+				    	synchronized(jobs)
+						{
+				    		//if all slaves are working, give to 1st working slave and move it to the last position in working slaves			    		
+				    		first.addJob(jobs.removeFirst());					    		
+						}	
+				    	System.out.println("Sent job to thread "+first.getID());
+				    	workingSlaves.addLast(first);
+				    }												    				
+				}
+				else
+				{
+					//redistribute?
+				}
+			}	 		
 				
-			    if (!idleSlaves.isEmpty())
-			    {
-			    	synchronized(jobs)
-					{
-			    		idleSlaves.getFirst().addJob(jobs.removeFirst());
-					}
-			    	workingSlaves.add(idleSlaves.removeFirst());
-			    }
-			    
-			    else
-			    {
-			    	synchronized(jobs)
-					{
-			    		//if all slaves are working, give to 1st working slave and move it to the last position in working slaves
-			    		SlaveServerThread first = workingSlaves.removeFirst();
-			    		first.addJob(jobs.removeFirst());
-			    		workingSlaves.addLast(first);
-					}
-			    	
-			    }			
-								    				
-			}
-		}
-			
 	}
-	
 	
 	//method for the slave/slaveserverthread to tell the threadedserver when it's done. 
 	// How will the slave/slaveserverthread call this method?
