0301b47e3508e20e2065fbf36ac313ec35feebc4
dklahr@student.touro.edu <dklahr@student.touro.edu>
2018-05-27 14:38:26 -0400

    refactored SlaveServerThread.java

--- src/SlaveServerThread.java
+++ src/SlaveServerThread.java
@@ -1,84 +1,64 @@
 import java.io.*;
 import java.net.*;
+import java.util.LinkedList;
 
 public class SlaveServerThread extends Thread{
 
+	private int id;
 	private String IPAddress;
 	private int portNumber;
-	private int id;
-	private String currentJob;
-	private PrintWriter outputStream;
-	private BufferedReader requestReader;
+	private LinkedList<String> jobs;
 	private SlaveThreadedServer myThreadedServerBoss;
+	private PrintWriter outputStream;
 	
-	public SlaveServerThread(String IPAddress, int portNumber, int id, SlaveThreadedServer myThreadedServerBoss)
+	public SlaveServerThread(String IPAddress, int portNumber, int id, SlaveThreadedServer parent)
 	{
+		this.id = id;
 		this.IPAddress = IPAddress;
-		this.portNumber = portNumber;
-		this.id =id;
-		this.myThreadedServerBoss=myThreadedServerBoss;
-		
-		
-		//set up connection
-				try(Socket slaveSocket = new Socket(IPAddress, portNumber);
-						)				
-					{	
-						outputStream =new  PrintWriter(slaveSocket.getOutputStream(), true);
-						requestReader = new BufferedReader(new InputStreamReader(slaveSocket.getInputStream()));
-					
-					}
-				catch(UnknownHostException e)
-				{
-					e.printStackTrace();
-				}
-						
-				 catch (IOException f) {
-					
-					System.out.println(f.getMessage());
-				}
-		
-		
+		this.portNumber = portNumber;	
+		this.myThreadedServerBoss = parent;
+		this.jobs = new LinkedList<String>();
 	}
 	
 
 	@Override
 	public void run() 
 	{					
-		String slaveDoneMessage;
-		
-		try			
-			{	
+		//set up connection
+		try(Socket slaveSocket = new Socket(IPAddress, portNumber);
+			//PrintWriter outputStream = new  PrintWriter(slaveSocket.getOutputStream(), true);
+			BufferedReader requestReader = new BufferedReader(new InputStreamReader(slaveSocket.getInputStream()));
+				)				
+		{	
+			outputStream = new  PrintWriter(slaveSocket.getOutputStream(), true);
+			System.out.println("SlaveServerThread initialized"); //println for testing
 			
-				//accepts communication from the slaveTaskThread when it completes all its tasks
-				while((slaveDoneMessage = requestReader.readLine()) !=null)
-				{
-					//messages its boss - the threaded server - so it can know that a slave is idle
-					myThreadedServerBoss.slaveDoneMessage(getID());
-				}
+			String done;
+			while((done = requestReader.readLine()) != null)
+			{
+				myThreadedServerBoss.slaveDoneMessage(id);
 			}
-		
+		}
 		catch(UnknownHostException e)
 		{
 			e.printStackTrace();
 		}
 				
-		 catch (IOException f) {
-			
+		 catch (IOException f) 
+		{
 			System.out.println(f.getMessage());
 		}
 	}	
 	
-	
-	
-	
-	public void addJob(String job)
-	{
-		this.currentJob = job;
-		outputStream.write(currentJob);
-	}
-	
 	public int getID()
 	{
 		return id;
+	}
+	
+	public void addJob(String job)
+	{
+		jobs.add(job);
+		outputStream.println(job);
+		System.out.println("Slave Thread "+id+" added job");
 	}
 }
